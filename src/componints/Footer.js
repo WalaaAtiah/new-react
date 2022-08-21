ff609f962697d4bf12e078c350d8d92b213e096d
WalaaAtiah <walaaatiah97@gmail.com>
2022-08-22 01:37:12 +0300

    first react

---
+++ src/componints/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+class Footer extends React.Component {
+  render(){
+    return(
+    <div>
+      <h3> Walaa' Atiyh</h3>
+    </div>
+    )
+  }
+}
+
+export default Footer;
---
+++ src/componints/HornedBeast.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+
+class HornedBeast extends React.Component {
+  render(){
+    return(
+    <div>
+      <h2>{this.props.title}</h2>
+      <p>{this.props.disc}</p>
+      <img src={this.props.img} alt='image'></img>
+      <style jsx >
+        {`
+          img {
+            width:600px;
+            height:400px; 
+          }
+        `}
+      </style>
+    </div>
+    )
+  }
+}
+
+export default HornedBeast;
