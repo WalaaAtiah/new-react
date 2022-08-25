798db157f2c1f22661b9d288c0775b97446db4a0
WalaaAtiah <walaaatiah97@gmail.com>
2022-08-25 13:43:55 +0300

    final version

--- src/App.js
+++ src/App.js
@@ -1,108 +1,120 @@
-import React from 'react';
-import Header from './componints/Header';
-import Footer from './componints/Footer';
-import Main from './componints/Main';
-import SelestedBeast from './componints/SelectedBeast'
-import Data from './componints/Data.json'
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-
+import React from "react";
+import Header from "./componints/Header";
+import Footer from "./componints/Footer";
+import Main from "./componints/Main";
+import SelestedBeast from "./componints/SelectedBeast";
+import Data from "./componints/Data.json";
+import Form from "react-bootstrap/Form";
+// import Button from "react-bootstrap/Button";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       newModule: "",
-      setshow:false,
-      selBeast :{},
-      selecthorn:Data,
-      numberHorn :0,
-      bbb:5
-
-      
+      setshow: false,
+      selBeast: {},
+      selecthorn: Data,
+      numberHorn: 0,
+      bbb: 5,
     };
   }
 
   Module = (name) => {
-    
     this.setState({
       newModule: this.state.newModule + "💕",
-      setshow:true,
-      selBeast:Data.find(item=>item.title===name)
-
+      setshow: true,
+      selBeast: Data.find((item) => item.title === name),
     });
   };
   handleClose = () => {
     this.setState({
       setshow: false,
-      
     });
   };
-  numhorn =(event) =>{
-    this.setState({
-      numberHorn :event.target.value,
+  numhorn = (event) => {
+    // event.preventDefault();
+
       
-              })
-      event.preventDefault();
-      console.log(this.state.numberHorn);
-      let a =Data.filter(item=>{
-         
-        switch(this.state.numberHorn){
-          case "1":
-          case "2": 
-            return (item.horns==this.state.numberHorn);
-          case "3"  :
-            return (item.horns>=this.state.numberHorn);
-          default:
-            return true;
-        }
-      })
+   let  x =parseInt(event.target.value);
+   let newarray =[];
 
-      this.setState({
-       
-        selecthorn:a
-                })
+   console.log("num of selected horn ", x);
+   console.log("the array",newarray);
 
-      console.log(a);
-    }
-  
+ newarray = Data.filter((item) => {
+   
 
+      switch (x) {
+        case 1:
+        case 2:
+          return item.horns === x
+        case 3:
+          return item.horns >= x
+        default:
+          return true;
+      }
+    });
 
+    console.log("new array after filter",newarray);
+    this.setState({
+      selecthorn: newarray,
+      numberHorn:x
+    });
 
-  render(){
-    return(
-    <div style={{backgroundColor:"rgb(178, 235, 235)"  ,textAlign:'center'}}>
-      <h1>newmodel :{this.state.newModule}</h1>
-     
-    <Header />
+    console.log(this.state.selecthorn);
+  };
 
-    <div style={{textAlign:'center' ,borderStyle:'dashed', borderWidth: "2px" ,borderColor:'blue' ,width:'50rem' ,padding:'10px' ,fontSize:'25px'}}>
-      <form onChange={this.numhorn}>
-    <Form.Group className="mb-3" >
-        <Form.Label htmlFor='select'>Numbers of Horns 🦄</Form.Label>
-        <Form.Select id='select' style={{fontSize:'25px' ,textAlign:'center'}} onClick={this.numhorn} >
-          <option >Please Choose Number Of Horn</option>
-          <option value={1} >One</option>
-          <option value={2}>Two</option>
-          <option value ={3} >more</option>
-        </Form.Select>
-        <Button variant="primary" type="submit" >
-        reset 
-      </Button>
-      </Form.Group>
-      <> select the beast have number of horns={this.state.numberHorn}</>
-      {/* <> {this.state.selecthorn} </>  */}
-    </form>
-    </div>
+  render() {
+    return (
+      <div
+        style={{ backgroundColor: "rgb(178, 235, 235)", textAlign: "center" }}
+      >
+        <h1>newmodel :{this.state.newModule}</h1>
+      
+        <Header />
 
+        <div
+          style={{
+            textAlign: "center",
+            borderStyle: "dashed",
+            borderWidth: "2px",
+            borderColor: "blue",
+            width: "50rem",
+            padding: "10px",
+            fontSize: "25px",
+          }}
+        >
+          <form>
+            <Form.Group className="mb-3">
+              <Form.Label htmlFor="select">Numbers of Horns 🦄</Form.Label>
+              <Form.Select
+                id="select"
+                style={{ fontSize: "25px", textAlign: "center" }}
+                onChange={this.numhorn}
+              >
+                <option>Please Choose Number Of Horn</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">more</option>
+              </Form.Select>
+              {/* <Button variant="primary" type="submit" >
+                 reset 
+                </Button> */}
+            </Form.Group>
+            <> select the beast have number of horns={this.state.numberHorn}</>
+          </form>
+        </div>
 
-    
-    <Main  Data={this.state.selecthorn} Module={this.Module} />
-    <Footer />
-    <SelestedBeast setshow={this.state.setshow} handleClose={this.handleClose} selBeast={this.state.selBeast}/>
-
-    </div>
-    )
+        <Main Data={this.state.selecthorn} Module={this.Module} />
+        <Footer />
+        <SelestedBeast
+          setshow={this.state.setshow}
+          handleClose={this.handleClose}
+          selBeast={this.state.selBeast}
+        />
+      </div>
+    );
   }
 }
 
--- src/componints/Main.js
+++ src/componints/Main.js
@@ -5,7 +5,9 @@ import Row from "react-bootstrap/Row";
 
 class Main extends React.Component {
 
-
+  hi = () => {
+    console.log("hi",this.props.Data)
+  };
   // constructor(props) {
   //   super(props);
   //   this.state = {
@@ -17,6 +19,7 @@ class Main extends React.Component {
   render() {
     return (
       <Row xs={1} md={3} className="g-4">
+        {this.hi()}
         {this.props.Data.map((user) => (
           <HornedBeast
             id={user._id}
@@ -26,7 +29,7 @@ class Main extends React.Component {
             keyword={user.keyword}
             horns={user.horns}
             Module={this.props.Module}
-            
+            key={user._id}
           />
         ))}
       </Row>
